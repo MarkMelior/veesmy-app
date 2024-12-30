fa4ab123fed34d306ae2ece671e1bdb96e34ca33
Mark Melior <zavgorodniymv@sber-service.ru>
2024-12-30 11:40:20 +0300

    VEES-1 | Update stylelint

---
+++ .stylelintrc.mjs
@@ -0,0 +1,30 @@
+/** @type {import('stylelint').Config} */
+const config = {
+  extends: [
+    'stylelint-config-tailwindcss',
+    'stylelint-config-standard',
+  ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'function-no-unknown': [
+      true,
+      {
+        ignoreFunctions: ['theme'],
+      },
+    ],
+    'media-query-no-invalid': null,
+  },
+};
+
+export default config;
