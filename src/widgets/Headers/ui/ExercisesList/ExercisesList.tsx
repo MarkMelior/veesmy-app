6705a2e381e419914c3a0ff8660d2c36e9c9d9c0
Mark Melior <zavgorodniymv@sber-service.ru>
2025-01-06 18:57:34 +0300

    VEES-16 | Fix ts lint

--- src/widgets/Headers/ui/ExercisesList/ExercisesList.tsx
+++ src/widgets/Headers/ui/ExercisesList/ExercisesList.tsx
@@ -1,6 +1,7 @@
 import { AddExerciseModal } from '@/widgets/AddExerciseModal/client';
 import { mockAddExerciseModal } from '@/widgets/AddExerciseModal/model/mock';
 
+import { AppRoutes } from '@/shared/constants';
 import { ArrowLeftIcon, FinishOutlineIcon } from '@/shared/icons';
 import { Button, Header } from '@/shared/ui';
 
@@ -8,7 +9,7 @@ export const ExercisesList = () => (
   <Header
     leftActions={(
       <>
-        <Button iconOnly={true} to="/">
+        <Button iconOnly={true} to={AppRoutes.MAIN}>
           <ArrowLeftIcon />
         </Button>
         <Button iconOnly={true}>
